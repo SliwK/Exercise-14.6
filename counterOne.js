5e5713d84609f5eb3b2d7a775d29ed412a9eb43f
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-01 01:30:46 +0100

    Change js

--- counterOne.js
+++ counterOne.js
@@ -1,12 +1,13 @@
 var CounterOne = React.createClass({
     getInitialState: function() {
+        console.log("tu można ustawić początkowy stan licznika - jeśli jest");
         return {
             counter: 0
         };
     },
 
     getDefaultProps: function() {
-      console.log('GetDefaultProps');
+      console.log('GetDefaultProps - ustawiamy domyślne propsy');
     },
 
     increment: function() {
@@ -20,20 +21,20 @@ var CounterOne = React.createClass({
     },
 
     componentDidMount: function() {
-          console.log('ComponentDidMount - tu można wykonać coś na DOM - prezentujemy komponent użytkownikowi');
+          console.log('ComponentDidMount - tu można wykonać coś na DOM - prezentujemy komponent użytkownikowi. Każda zmiana setState powoduje aktualizacje i prezentacje');
       },
 
     componentWillReceiveProps: function(nextProps) {
-          console.log('ComponentWillRecieveProps - tu skoro przekazujemy te propsy to można w tym momencie porównać czy się rzeczywiscie zmieniły');
+          console.log('ComponentWillRecieveProps - tu będziemy gdy przekażemy nowe propsy - skoro przekazujemy te propsy to można w tym momencie porównać czy się rzeczywiscie zmieniły');
       },
 
     shouldComponentUpdate: function() {
-          console.log('ShouldComponentUpdate - tutaj można wstrzymać renderowanie zmian - czyli nie renderować skoro nie trzeba');
+          console.log('ShouldComponentUpdate - tutaj można wstrzymać renderowanie zmian - czyli (false) nie renderować skoro nie trzeba');
           return true;
       },
 
     componentWillUpdate: function() {
-          console.log('ComponentWillUpdate - nie znalazłem zastosowania :(');
+          console.log('ComponentWillUpdate - komponent sie zakualizuje - nie znalazłem zastosowania :(');
       },
 
     render: function() {
@@ -43,7 +44,7 @@ var CounterOne = React.createClass({
     },
 
     componentDidUpdate: function() {
-          console.log('ComponentDidUpdate - tutaj znowu można odnieść się do DOM i coś na nim wykonać');
+          console.log('ComponentDidUpdate - zaktualizował się - tutaj znowu można odnieść się do DOM i coś na nim wykonać');
       },
 
     componentWillUnmount: function() {
